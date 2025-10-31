c7939039bd230b43fa86c87fe5c3588ccbd1aa15
gowthamgandhari <gowthamgandhari88@gmail.com>
2025-10-31 13:58:18 +0530

    updated code in JF

--- Tf-Scrpt-Ec2-Eks-Swiggy-App/terraform-EKS-Code/locals.tf
+++ Tf-Scrpt-Ec2-Eks-Swiggy-App/terraform-EKS-Code/locals.tf
@@ -1,11 +1,11 @@
 locals {
   name            = "Swiggy-cluster"
   region          = "ap-south-1"
-  vpc_cidr        = "19.20.0.0/16"
+  vpc_cidr        = "19.30.0.0/16"
   azs             = ["ap-south-1a", "ap-south-1b"]
-  public_subnets  = ["19.20.40.0/24", "19.20.60.0/24"]
-  private_subnets = ["19.20.70.0/24", "19.20.90.0/24"]
-  intra_subnets   = ["19.20.1.0/24", "19.20.2.0/24"]
+  public_subnets  = ["19.30.40.0/24", "19.30.60.0/24"]
+  private_subnets = ["19.30.70.0/24", "19.30.90.0/24"]
+  intra_subnets   = ["19.30.1.0/24", "19.30.2.0/24"]
 }
 
 locals {
