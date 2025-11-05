299f5160bf7ebf244ff3cd32d0cc7ffee7324278
gowthamgandhari <gowthamgandhari88@gmail.com>
2025-11-05 12:54:06 +0530

    modified-locals.tf-eks

--- Tf-Scrpt-Ec2-Eks-Swiggy-App/terraform-EKS-Code/locals.tf
+++ Tf-Scrpt-Ec2-Eks-Swiggy-App/terraform-EKS-Code/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  name            = "Swiggy-cluster1"
+  name            = "My-Swiggy-cluster"
   region          = "ap-south-1"
   vpc_cidr        = "19.30.0.0/16"
   azs             = ["ap-south-1a", "ap-south-1b"]
